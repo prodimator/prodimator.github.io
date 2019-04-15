ba910648c27d64caa3a2146b94fda4562bbc6e63
Ryan <rwolfe22@gmail.com>
2019-04-15 14:18:50 -0700

    Create react app and initial setup

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import './header.scss';
+
+export default function Welcome() {
+    return (
+        <div className="header">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
